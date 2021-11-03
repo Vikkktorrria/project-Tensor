a8f11f0b917ae6bf6cd6b0538fcfd0a256fec829
Evgexajs <ekopytov509@gmail.com>
2021-11-04 00:16:59 +0500

    fix adaptive layout

--- Frontend/js/javascript.js
+++ Frontend/js/javascript.js
@@ -6,9 +6,9 @@ window.onload = function () {
             el: '.swiper-pagination',
             clickable: true,
         },
-        // autoplay: {
-        //   delay: 3000,
-        // },
+        autoplay: {
+          delay: 3000,
+        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
@@ -18,10 +18,10 @@ window.onload = function () {
 function navigation() {
     if (document.querySelector('.navigation').classList.contains('active')) {
         document.querySelector('.navigation').classList.remove('active')
-        // document.querySelector('.navigation__burger').classList.remove('active')
+        document.querySelector('.navigation__burger').classList.remove('active')
 
     } else {
         document.querySelector('.navigation').classList.add('active')
-        // document.querySelector('.navigation__burger').classList.add('active')
+        document.querySelector('.navigation__burger').classList.add('active')
     }
 }
