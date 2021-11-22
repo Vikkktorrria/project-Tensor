8824482ac1479db2d4cb1fd9e7f656b237e1ecbd
Vikkktorrria <91339795+Vikkktorrria@users.noreply.github.com>
2021-11-22 22:43:23 +0500

    Add files via upload
    classes for back (bd)

--- Backend/classes.py
+++ Backend/classes.py
@@ -1,141 +1,50 @@
 class PatientProfile:
     """Класс для работы с профилем пациента"""
     def __init__(self, fullName, passport, anamnesis):
-        self._fullName = fullName
-        self._passport = passport
-        self._anamnesis = anamnesis
-
-    @property
-    def fullName(self):
-        return self._fullName
-    @property
-    def passport(self):
-        return self._passport
-    @property
-    def anamnesis(self):
-        return self._anamnesis
-
-    @fullName.setter
-    def fullName(self, value):
-        self._fullName = value
-    @passport.setter
-    def passport(self, value):
-        self._passport = value
-    @anamnesis.setter
-    def anamnesis(self, value):
-        self._anamnesis = value
-    
-    def __str__(self):
-        return "Профиль пациента {name}, паспорт № {passport}".format(name=self.fullName, passport=self.passport)
+        self.fullName = fullName
+        self.passport = passport
+        self.anamnesis = anamnesis
 
 
 class DoctorProfile:
     """Класс для работы с профилем врача"""
-    def __init__(self, fullName, qualification, profiling):
-        self._fullName = fullName
-        self._qualification = qualification
-        self._profiling = profiling
-
-    @property
-    def fullName(self):
-        return self._fullName
-    @property
-    def qualification(self):
-        return self._qualification
-    @property
-    def profiling(self):
-        return self._profiling
-
-    @fullName.setter
-    def fullName(self, value):
-        self._fullName = value
-    @qualification.setter
-    def qualification(self, value):
-        self._qualification = value
-    @profiling.setter
-    def profiling(self, value):
-        self._profiling = value
-
-    def __str__(self):
-        return "Врач- {profiling} {name}".format(profiling=self.profiling, name=self.fullName)\
-               + " " + "Квалификация: {qualification}".format(qualification=self.qualification)
+    def __init__(self, fullName, qualification):
+        self.fullName = fullName
+        self.qualification = qualification
 
 
 class Diagnosis:
     """Класс диагнозов"""
     def __init__(self, infAboutDiagnos):
-        self._infAboutDiagnos = infAboutDiagnos
-
-    @property
-    def infAboutDiagnos(self):
-        return self._infAboutDiagnos
-
-    @infAboutDiagnos.setter
-    def infAboutDiagnos(self, value):
-        self._infAboutDiagnos = value
-
-    def __str__(self):
-        return "Диагноз: {diagnos_inf}".format(diagnos_inf=self.infAboutDiagnos)
+        self.infAboutDiagnos = infAboutDiagnos
 
 
 class Recipe:
     """Класс рецептов"""
     def __init__(self, infAboutRecipe):
-        self._infAboutRecipe = infAboutRecipe
-
-    @property
-    def infAboutRecipe(self):
-        return self._infAboutRecipe
-
-    @infAboutRecipe.setter
-    def infAboutRecipe(self, value):
-        self._infAboutRecipe = value
-
-    def __str__(self):
-        return "Рецепт: {recipe}".format(recipe=self.recipe)
+        self.infAboutRecipe = infAboutRecipe
 
 
-class Note:
+class Note(Diagnosis, Recipe):
     """Класс для работы записями на приём"""
-    def __init__(self, date, information, Recipe, Diagnosis):
-        self._date = date
-        self._information = information
-        self.recipe = Recipe
-        self.diagnosis = Diagnosis
-
-    @property
-    def date(self):
-        return self._date
-    @property
-    def information(self):
-        return self._information
-
-    @date.setter
-    def date(self, value):
-        self._date = value
-    @information.setter
-    def information(self, value):
-        self._information = value
-
-    def __str__(self):
-        return "Приём назначен на {date}".format(date=self.date) + "Информация о приёме: {info}".format(info=self.information)
+    def __init__(self, date, information):
+        self.date = date
+        self.information = information
 
 
-class User:
-    """Класс для работы с пользователем (пациент или врач)"""
+class Patient:
+    """Класс для работы с пациентом"""
+    profile = PatientProfile()  # записали классы как атрибуты
     notes = []
 
-    def __init__(self, Profile):
-        self._profile = Profile # записываем объект класса как атрибут
-        
-    @property
-    def profile(self):
-        return self._profile
-    
-    @profile.setter
-    def profile(self, value):
-        self._profile = value
-
-    def addNote(self, other, Note):
-        other.notes.append(Note)
+    def addNote(self, Note):
         self.notes.append(Note)
+
+
+class Doctor:
+    """Класс для работы с врачом"""
+    profile = DoctorProfile()
+    note = Note()
+
+    def addNote(patient, Note):
+        patient.notes.append(Note)
