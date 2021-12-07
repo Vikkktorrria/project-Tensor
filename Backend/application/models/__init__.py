eac04d162c3db448afdcc56fe12e7fafef032956
mautaliev <92651578+mautaliev@users.noreply.github.com>
2021-12-08 00:53:03 +0500

    Delete Backend directory

--- Backend/application/models/__init__.py
+++
@@ -1,7 +0,0 @@
-import application.models.user_model
-import application.models.article_model
-import application.models.passport_model
-import application.models.doctor_model
-import application.models.note_model
-import application.models.passport_model
-import application.models.snils_model
--- Backend/application/route/__init__.py
+++
@@ -1,5 +0,0 @@
-import application.route.user
-import application.route.auth
-import application.route.article
-import application.route.note
-import application.route.doctor
