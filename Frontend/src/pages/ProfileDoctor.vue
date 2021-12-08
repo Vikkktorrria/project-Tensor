a106dcae37c4faed33262ef62bebd14dddc2838a
Evgexajs <ekopytov509@gmail.com>
2021-12-08 19:05:53 +0500

    profile doctor

---
+++ Frontend/src/pages/ProfileDoctor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <head-profile></head-profile>
+  </div>
+</template>
+
+<script>
+import HeadProfile from "../components/Profile/HeadProfile";
+export default {
+  name: "ProfileDoctor",
+  components: {
+    HeadProfile
+  }
+}
+</script>
+
+<style scoped>
+</style>
